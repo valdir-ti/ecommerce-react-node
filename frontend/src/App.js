26e339e2958dc2f72e3d21ec1154b8b4f72dc1e8
valdir-ti <valdir.ti@gmail.com>
2020-08-04 23:22:51 -0300

    Rendering products like a JSX element from data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import data from './data';
 
 function App() {
 
@@ -46,83 +47,19 @@ function App() {
       <div className="content">
         <ul className="products">
           
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$65</div>
-              <div className="product-rating">4.5 Stars (10 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 2</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$65</div>
-              <div className="product-rating">4.2 Stars (44 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 3</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$65</div>
-              <div className="product-rating">4.9 Stars (65 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 4</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$65</div>
-              <div className="product-rating">3.5 Stars (6 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 5</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$65</div>
-              <div className="product-rating">4.1 Stars (20 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 6</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$60</div>
-              <div className="product-rating">4 Stars (16 Reviews)</div>
-            </div>
-          </li>
-          <li>
-            <div className="product">
-              <img src="/images/d1.jpg" alt="product" className="product-image" />
-              <div className="product-name">
-                <a href="product.html">Slim shirt 7</a>
-              </div>            
-              <div className="product-brand">Nike</div>             
-              <div className="product-price">$63</div>
-              <div className="product-rating">5 Stars (14 Reviews)</div>
-            </div>
-          </li>
+          { data.products.map(product => 
+              <li>
+                <div className="product">
+                  <img src={product.image} alt="product" className="product-image" />
+                  <div className="product-name">
+                    <a href="product.html">{product.name}</a>
+                  </div>            
+                  <div className="product-brand">{product.brand}</div>             
+                  <div className="product-price">${product.price}</div>
+                  <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+                </div>
+              </li>
+          )}
 
         </ul>
       </div>
