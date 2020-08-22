779e4844572e98d658b634399e8df452a4db2a10
valdir-ti <valdir.ti@gmail.com>
2020-08-22 17:51:34 -0300

    Creating a Node Server

---
+++ backend/data.js
@@ -0,0 +1,74 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Slim Shirt',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 64,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      name: 'Slim Shirt 2',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 40,
+      brand: 'Nike',
+      rating: 5.0,
+      numReviews: 15
+    },
+    {
+      _id: '3',
+      name: 'Fit Shirt 3',
+      category: 'Shirts',
+      image: '/images/d1.jpg',
+      price: 23,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 34
+    },
+    {
+      _id: '4',
+      name: 'Best Pants 1',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 76,
+      brand: 'Lacoste',
+      rating: 3.8,
+      numReviews: 100
+    },
+    {
+      _id: '5',
+      name: 'Best Pants 2',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 76,
+      brand: 'Lacoste',
+      rating: 3.8,
+      numReviews: 100
+    },
+    {
+      _id: '6',
+      name: 'Best Pants 3',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 76,
+      brand: 'Lacoste',
+      rating: 3.8,
+      numReviews: 100
+    },
+    {
+      _id: '7',
+      name: 'Best Pants 4',
+      category: 'Pants',
+      image: '/images/d1.jpg',
+      price: 76,
+      brand: 'Lacoste',
+      rating: 3.8,
+      numReviews: 100
+    },
+  ]
+}
