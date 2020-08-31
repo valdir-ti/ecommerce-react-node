bceb199a9361a845d1aacf115978842411c39d94
valdir-ti <valdir.ti@gmail.com>
2020-08-31 01:06:43 -0300

    Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config'; 
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+
+mongoose.connect(mongodbUrl,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 const port = 5001;
 app.use(cors());
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products",(req, res) => {
   res.send(data.products);
